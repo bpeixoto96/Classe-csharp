77bb8d6f7f2aed8e655d2437ea3610771ce384b4
bpeixoto96 <peixotobruno17@gmail.com>
2022-07-27 21:59:24 -0300

    Adicionar arquivos de projeto.

---
+++ Classe csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classe_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oi, eu sou o goku!");
+        }
+    }
+}
