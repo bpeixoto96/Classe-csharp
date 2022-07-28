75a4c50d9d205f5dff239451a3b3b7b12ddb1cbd
bpeixoto96 <peixotobruno17@gmail.com>
2022-07-27 22:58:54 -0300

    classe

--- Classe csharp/Program.cs
+++ Classe csharp/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using static System.Console;
 
-namespace Classe_csharp
+namespace Projeto_Classe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Oi, eu sou o goku!");
+            WriteLine("Bem vindo!");
+            WriteLine("Para listar nossos veículos digite 1");
+            WriteLine("Para sair digite 0");
+            int opUser = int.Parse(ReadLine());
+
+
+            Carro Supra = new Carro("Supra MK3", "2JZ Turbo", "Toyota", "1997");
+            Carro GTR34 = new Carro("Skyline GTR34", "RB26 Turbo", "Nissan", "2000");
+            Carro RX7 = new Carro("RX7", "13B-REW", "Mazda", "1997");
+            Carro Impreza = new Carro("Impreza WRX STI", "EJ255", "Subaru", "2000");
+
+            WriteLine(Supra.ToString());
+            WriteLine(GTR34.ToString());
+            WriteLine(RX7.ToString());
+            WriteLine(Impreza.ToString());
+            WriteLine("\nAplicativo finalizado\n");
         }
     }
 }
